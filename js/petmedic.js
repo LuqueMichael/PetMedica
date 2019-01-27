d04f439d9c71d672d458f0440cd6cda3ca13550e
Luque <michael.luque@qnexapp.com>
2019-01-27 12:57:52 -0500

    add modal in page product-detail, add social share, add filter animation in petmedic pro list

--- js/petmedic.js
+++ js/petmedic.js
@@ -32,6 +32,25 @@ $(function () {
         cycle: true
     });
 
+    //FILTER ROWS
+    $("#card-submenu").find('.nav-link').on("mouseover", function () {
+        var value = $(this).attr('data-target');
+        $('#card-submenu').find('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        if(value=='todo'){
+            $(".post").removeClass('hide');
+        }else{
+            $(".post").filter(function () {
+                let flag = $(this).attr('data-target').toLowerCase().indexOf(value) > -1;
+                if(flag){
+                    $(this).removeClass('hide');
+                }else{
+                    $(this).addClass('hide');
+                }
+            });
+        }
+    });
+
 
 });
 
@@ -39,38 +58,38 @@ $(function () {
     "use strict";
     // Auto-scroll
     $('#productos-destacados').carousel({
-      interval: 10000
+        interval: 10000
     });
-  
+
     // Control buttons
     $('#productos-destacados .next').click(function () {
-      $('.carousel').carousel('next');
-      return false;
+        $('.carousel').carousel('next');
+        return false;
     });
     $('#productos-destacados .prev').click(function () {
-      $('.carousel').carousel('prev');
-      return false;
+        $('.carousel').carousel('prev');
+        return false;
     });
-  
+
     // On carousel scroll
     $("#productos-destacados").on("slide.bs.carousel", function (e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $(".carousel-item").length;
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide -
-            (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-          // append slides to end 
-          if (e.direction == "left") {
-            $(
-              ".carousel-item").eq(i).appendTo(".carousel-inner");
-          } else {
-            $(".carousel-item").eq(0).appendTo(".carousel-inner");
-          }
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $(".carousel-item").length;
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide -
+                (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end 
+                if (e.direction == "left") {
+                    $(
+                        ".carousel-item").eq(i).appendTo(".carousel-inner");
+                } else {
+                    $(".carousel-item").eq(0).appendTo(".carousel-inner");
+                }
+            }
         }
-      }
     });
-  })
-  (jQuery);
+})
+(jQuery);
