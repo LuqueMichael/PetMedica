3ee4ede64ed6bf292e46d64fc2d94678322c90c7
Luque <michael.luque@qnexapp.com>
2019-03-19 17:37:03 -0500

    fix-XS responsive

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,6 @@
-$(document).on('ready',function(){
-    //RESPONSIVE MENU
-    $('.btn-white').on('click',function(){
-        console.log('lcick');
-        $(this).parent().find('ul').show()
-    });
+
+//RESPONSIVE MENU
+$('.r-menu').click(function(){
+    console.log('lcick');
+    $(this).parent().find('ul').slideToggle();
 });
